19f49b7ddc2bad320849850e63f16ded45d3ec6f
Edwin Oganga <edwinjoganga@gmail.com>
2023-10-08 13:14:46 +0300

    Update settings.py

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)$^2#&(%@i#ww5qc)jbq7tlge$9hh@c3mje(s%_cqs%ng2p^w*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
