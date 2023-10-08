115127d5f7877c05511553a5079ec346ff8c233e
Edwin Oganga <edwinjoganga@gmail.com>
2023-10-08 13:32:48 +0300

    Update settings.py

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)$^2#&(%@i#ww5qc)jbq7tlge$9hh@c3mje(s%_cqs%ng2p^w*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
